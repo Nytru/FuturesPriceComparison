b6fc5ddb9fd1087bbf0ea72a6a754640e7300930
yan <yan@nytru.com>
2025-03-29 03:00:09 +0300

    new namespace

--- FuturesPriceComparison.PriceChecker/Program.cs
+++ FuturesPriceComparison.PriceChecker/Program.cs
@@ -1,11 +1,20 @@
+using FuturesPriceComparison.ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
+
 // Add services to the container.
+builder.Services.AddProblemDetails();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
